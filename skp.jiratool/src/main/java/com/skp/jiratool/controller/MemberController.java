85bfa3723c126ed2fe6aec1106d491d9156a651a
Min-Soo Kim <Min-Soo Kim@DESKTOP-39UI6ET>
2023-04-26 16:24:52 +0900

    hi

---
+++ skp.jiratool/src/main/java/com/skp/jiratool/controller/MemberController.java
@@ -0,0 +1,14 @@
+package com.skp.jiratool.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MemberController {
+
+	@GetMapping("/member/login")
+	public String login() {
+		return "/member/login";
+	}
+	
+}
